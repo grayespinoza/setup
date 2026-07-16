30635058ff940f8697a50de321a1836569b02641
Grey <grey@grayespinoza.dev>
2026-07-16 11:15:18 -0700

    fix(Dotfiles): correct source paths

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,12 +5,12 @@ cd ~/.repo
 git clone https://github.com/grayespinoza/.dotfiles.git
 cd .dotfiles
 
-ln -s .bashrc ~/
-ln -s .gitconfig ~/
-ln -s .wezterm.lua ~/
+ln -s ~/.repo/.dotfiles/.bashrc ~/
+ln -s ~/.repo/.dotfiles/.gitconfig ~/
+ln -s ~/.repo/.dotfiles/.wezterm.lua ~/
 
-ln -s .config/hyprland ~/.config/
-ln -s .config/mpd ~/.config/
-ln -s .config/mpv ~/.config/
-ln -s .config/nvim ~/.config/
-ln -s .config/yazi ~/.config/
+ln -s ~/.repo/.dotfiles/.config/hypr ~/.config/
+ln -s ~/.repo/.dotfiles/.config/mpd ~/.config/
+ln -s ~/.repo/.dotfiles/.config/mpv ~/.config/
+ln -s ~/.repo/.dotfiles/.config/nvim ~/.config/
+ln -s ~/.repo/.dotfiles/.config/yazi ~/.config/
